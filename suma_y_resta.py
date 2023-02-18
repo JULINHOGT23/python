6b64c00e90cbd39ce85bc4cd2641c82f587c79ea
unknown <JULINHOGT23>
2023-02-18 09:52:38 -0500

    suma y resta

---
+++ suma_y_resta.py
@@ -0,0 +1,8 @@
+# suma y resta  en python
+
+# ingresamos los valores a restar
+
+n1 = int(input("ingrese el primer valor 1 : "))
+n2 = int(input("ingrese el segundo valor 2 : "))
+n3 = int(input("ingrese el segundo valor 3 : "))
+
